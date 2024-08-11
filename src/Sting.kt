607b175b04b3a2d9c4bb37751d89fe5f74ca7042
Ahmed Khaled <a7medalqalawyi@gmail.com>
2024-08-11 22:08:27 +0300

    Adding Strings file

---
+++ src/Sting.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var username: String ="Ahmed khaled"
+    println(username)
+    println(username.length)
+    println(username.indexOf('s'))
+    println(username.indexOf('m'))
+    println(username.substring(3))
+
+}
