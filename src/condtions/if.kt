d5ca26819405935735db91cab4e6dadec2628475
Ahmed Khaled <a7medalqalawyi@gmail.com>
2024-08-13 13:46:33 +0300

    Added if condition

---
+++ src/condtions/if.kt
@@ -0,0 +1,12 @@
+val isUserBlocked=false
+fun main(){
+    if (isUserBlocked){
+        println("User Blocked")
+    }else
+        println("User not blocked")
+
+    val number1=200
+    val number2=280
+    var max= if(number1>number2)   number1 else  number2
+    println(max)
+}
